58d7a446f14530d9fa4f8c41bbdb79032498dbde
Jason P. Kaplan <JasonPKaplan@live.ca>
2016-05-30 11:27:57 -0400

    Added UIImageAnimation script from deprecated repo

---
+++ Assets/CustomAssets/Scripts/UIImageAnimation.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UIImageAnimation : MonoBehaviour {
+
+	public Sprite[] SpriteAnimation;
+	public int FPS = 24;
+	private Image _Image;
+	private int _Index = 0;
+	private float _ElapsedTime;
+
+	void Start ()
+	{
+		_Image = this.GetComponent<Image> ();
+		if (SpriteAnimation.Length > 0 && SpriteAnimation [0] != null)
+		{
+			_Image.sprite = SpriteAnimation [0];
+		}
+	}
+	
+	void Update ()
+	{
+		_ElapsedTime += Time.deltaTime;
+		if (_ElapsedTime > 1.0f / FPS)
+		{
+			_ElapsedTime -= 1.0f / FPS;
+			_Index++;
+			if (_Index >= SpriteAnimation.Length)
+			{
+				_Index = 0;
+			}
+			_Image.sprite = SpriteAnimation [_Index];
+		}
+	}
+}
